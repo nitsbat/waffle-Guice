0349cad713043e853de537ab21111218682909c9
Nitin Bisht <nitin.bisht@iongroup.com>
2019-10-01 16:54:18 +0530

    Dependencies injected.

--- GUICE/src/main/java/com/drawingapp/main/GuiceDemo.java
+++ GUICE/src/main/java/com/drawingapp/main/GuiceDemo.java
@@ -1,24 +1,23 @@
 package com.drawingapp.main;
 
-import com.drawingapp.requests.SquareReuest;
+import com.drawingapp.module.AppModule;
+import com.drawingapp.requests.SquareRequest;
 import com.drawingapp.services.DrawShape;
-import com.drawingapp.services.DrawSquare;
+import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class GuiceDemo {
 
-	private static final String SQUARE_REQ = "SQUARE";
+    private static final String SQUARE_REQ = "SQUARE";
 
-	public static void main(String[] args) {
-		sendRequest(SQUARE_REQ);
-	}
+    public static void main(String[] args) {
+        sendRequest(SQUARE_REQ);
+    }
 
-	private static void sendRequest(String squareReq) {
-//		DrawShape d = new DrawSquare();
-		Injector injector = null;
-		DrawShape d = injector.getInstance(DrawShape.class); 
-		SquareReuest request = new SquareReuest(d);
-		request.makeRequest();
-	}
+    private static void sendRequest(String squareReq) {
+        Injector injector = Guice.createInjector(new AppModule());
+        SquareRequest request = injector.getInstance(SquareRequest.class);
+        request.makeRequest();
+    }
 
 }
