37d648316ef43cfc5fa464a3ee502990bef772fb
nits <3498georgian@gmail.com>
2019-10-07 22:58:18 +0530

    Added The Constant Dependencies

--- guice.basics/src/main/java/guice/basics/DrawSquare.java
+++ guice.basics/src/main/java/guice/basics/DrawSquare.java
@@ -1,8 +1,24 @@
 package guice.basics;
 
-public class DrawSquare implements DrawShape {
+import com.google.inject.Inject;
 
+import guice.annotations.ColorValue;
+import guice.annotations.EdgeValue;
+
+public class DrawSquare implements DrawShape {
+	
+
+	private String color;
+	private int edge;
+		
+	@Inject
+	public DrawSquare(@ColorValue String color,@EdgeValue int edge) {
+		super();
+		this.color = color;
+		this.edge = edge;
+	} 
+	
 	public void draw() {
-		System.out.println("Inside the Drawing app");
+		System.out.println("Inside the Drawing app with color "+color+ " and edge "+edge);
 	}
 }
--- guice.basics/src/main/java/guice/module/AppModule.java
+++ guice.basics/src/main/java/guice/module/AppModule.java
@@ -2,15 +2,18 @@ package guice.module;
 
 import com.google.inject.AbstractModule;
 
+import guice.annotations.ColorValue;
+import guice.annotations.EdgeValue;
 import guice.basics.DrawShape;
 import guice.basics.DrawSquare;
-import guice.services.SquareRequest;
 
-public class AppModule extends AbstractModule{
+public class AppModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
 		bind(DrawShape.class).to(DrawSquare.class);
+		bind(String.class).annotatedWith(ColorValue.class).toInstance("Yellow");
+		bind(Integer.class).annotatedWith(EdgeValue.class).toInstance(50);
 	}
 
 }
