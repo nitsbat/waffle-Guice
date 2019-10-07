ff89e0c4377a609808e4db7f839d045ba79e47cb
nits <3498georgian@gmail.com>
2019-10-07 22:06:18 +0530

    Subclass Removed

--- guice.basics/src/main/java/guice/module/AppModule.java
+++ guice.basics/src/main/java/guice/module/AppModule.java
@@ -4,7 +4,6 @@ import com.google.inject.AbstractModule;
 
 import guice.basics.DrawShape;
 import guice.basics.DrawSquare;
-import guice.services.DrawSquareRequestSubClass;
 import guice.services.SquareRequest;
 
 public class AppModule extends AbstractModule{
@@ -12,7 +11,6 @@ public class AppModule extends AbstractModule{
 	@Override
 	protected void configure() {
 		bind(DrawShape.class).to(DrawSquare.class);
-		bind(SquareRequest.class).to(DrawSquareRequestSubClass.class);
 	}
 
 }
